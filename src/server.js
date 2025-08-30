a70da398e38e5c0a8bb7770c2480790aa86434da
Gigio <gigio.m.melo@gmail.com>
2025-08-29 21:55:39 -0300

    starting project, http methods, http status code, understading streams on node.js

---
+++ src/server.js
@@ -0,0 +1,56 @@
+import http from 'node:http'
+
+// - Criar usuários
+// - Listar usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+// - Método HTTP
+// - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Busca uma recurso no back-end
+// POST => Criar um recurso
+// PUT => Editar ou atualizar um recurso no back-end
+// PATCH => Atualizar uma informação única ou específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários do back-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless (Salva ou não em memória)
+
+// JSON - JavaScript Object Notation
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
+
+const users = []
+
+const server = http.createServer((req,res) => {
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+        // Early return
+        return res
+        .setHeader('Content-type','application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
+})
+
+server.listen(3333)
