d9fe543da90aed4095f1eac8a378fc6e0390624b
Gigio <gigio.m.melo@gmail.com>
2025-09-03 16:13:24 -0300

    writable and transfom streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -11,3 +11,47 @@
 // 10mb/s -> 10.000 linhas
 
 // Readable Streams / Writeable Streams
+
+// Streams ->
+
+//process.stdin
+    //.pipe(process.stdout) 
+
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OnetoHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if(i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class InverseNumberStream extends Transform{
+    _transform(chunk, enconding, callback){
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, enconding, callback){
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new OnetoHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
