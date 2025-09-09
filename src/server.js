b4266a1abfef99a7f77a7822b2840b2c82231e17
Gigio <gigio.m.melo@gmail.com>
2025-09-09 14:32:33 -0300

    JSON database created

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
 // - Criar usuários
 // - Listar usuários
@@ -29,7 +30,7 @@ import { json } from './middlewares/json.js'
 
 // HTTP Status Code
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req,res) => {
     const { method, url } = req
@@ -37,19 +38,22 @@ const server = http.createServer(async (req,res) => {
     await json(req,res)
 
     if (method === 'GET' && url === '/users') {
+        const users = database.select('users')
+
         // Early return
-        return res
-        .end(JSON.stringify(users))
+        return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
         const { name, email } = req.body
 
-        users.push({
+        const user = {
             id: 1,
             name,
             email,
-        })
+        }
+
+        database.insert('users', user)
 
         return res.writeHead(201).end()
     }
