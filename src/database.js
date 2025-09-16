f84302f5a7ebacb3a61bb3bd552c35c4452d5bfd
Gigio <gigio.m.melo@gmail.com>
2025-09-16 16:26:07 -0300

    filter list from database

--- src/database.js
+++ src/database.js
@@ -21,8 +21,19 @@ export class Database {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    select(table) {
-        const data = this.#database[table] ?? []
+    // {name: "Gigio", email: "Gigio"}
+    // [['name','Gigio'], ['email','Gigio']]
+
+    select(table, search) {
+        let data = this.#database[table] ?? []
+
+        if (search) {
+            data = data.filter( row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+            })
+        }
 
         return data
     } 
--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,12 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/users'),
         handler: (req, res) => {
-            console.log(req.query)
+            const { search } = req.query
 
-            const users = database.select('users')
+            const users = database.select('users', search ? {
+                name: search,
+                email: search,
+            } : null)
 
             // Early return
             return res.end(JSON.stringify(users))
